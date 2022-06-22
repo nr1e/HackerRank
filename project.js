d2a14dfa1d31df9636b4a48c960bfbbb9d127422
Narek Asaqtryan <narasatryan03@gmail.com>
2022-06-22 13:39:15 +0400

    5

--- project.js
+++ project.js
@@ -1,25 +1,27 @@
-var arr = [1,2,3,4,3,2,1]
+var arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+function diagonalDifference(arr){
+    var m=0;
+    var q=0;
+    var n=0;
+    var k = arr.length
+    for(var i = 0; i < k; i++){
+        n+=arr[i][i];
+    }
+    for(var i = 0, j=k-1; i < k, j >=0; i++, j--){
+        m+=arr[i][j];
+    }
+    if(m-n < 0){
+        q = n-m;    
+    }
+    else{
+        q = m-n;
+    }
 
-function lonelyInteger(arr){
-    var arr1 = [];
-    var n = arr.length;
-    var k = 0;
-    for(var i = 0; i < n; i++){
-        arr1[i]=0;    
-    }
-    for(var i = 0; i < n; i++){
-        for(var j = 0; j < n ; j++){
-            if(arr[i]==arr[j]){
-                arr1[i]++;
-            }      
-        }        
-    }
-    for(var i = 0; i < n; i++){
-        if(arr1[i]==1){
-            k = arr[i];    
-        }   
-    }
-    console.log(k)
+    console.log(q)
 }
 
-lonelyInteger(arr);
+diagonalDifference(arr)
